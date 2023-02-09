f5995385cb5f145819b2f68d6065c7e620992509
Appleiris <57938200+techbabette@users.noreply.github.com>
2023-02-09 06:37:44 +0100

    Hour long persistence
    Possibly a security risk, only used for practice

--- server/login.php
+++ server/login.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('session.gc_maxlifetime', 3600);
+session_set_cookie_params(3600);
 session_start();
 $correctUsername = "admin";
 $correstPassword = "admin";
--- server/logout.php
+++ server/logout.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('session.gc_maxlifetime', 3600);
+session_set_cookie_params(3600);
 session_start();
 session_destroy();
 $response['success'] = true;
